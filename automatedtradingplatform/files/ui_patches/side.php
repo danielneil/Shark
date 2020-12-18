39127d8c7d70f4d8e80cec6613cc70e74625013f
Daniel Neil <danielneilhodder@gmail.com>
2020-12-18 16:58:05 +1100

    added more ui changes - side bar

---
+++ automatedtradingplatform/files/ui_patches/side.php
@@ -0,0 +1,122 @@
+<?php
+include_once(dirname(__FILE__).'/includes/utils.inc.php');
+
+$this_version = '4.4.6';
+$link_target = 'main';
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<html>
+
+<head>
+<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+<title>Nagios Core</title>
+<link href="stylesheets/common.css?<?php echo $this_version; ?>" type="text/css" rel="stylesheet">
+</head>
+
+
+<body class='navbar'>
+
+<div class="navbarlogo">
+	<a href="https://www.nagios.org" target="_blank"><img src="images/sblogo.png" height="39" width="140" border="0" alt="Nagios" /></a>
+</div>
+
+<div class="navsection">
+	<div class="navsectiontitle">General</div>
+	<div class="navsectionlinks">
+		<ul class="navsectionlinks">
+			<li><a href="main.php" target="<?php echo $link_target;?>">Home</a></li>
+			<li><a href="https://go.nagios.com/nagioscore/docs" target="_blank">Documentation</a></li>
+		</ul>
+	</div>
+</div>
+
+<div class="navsection">
+	<div class="navsectiontitle">Current Status</div>
+	<div class="navsectionlinks">
+		<ul class="navsectionlinks">
+			<li><a href="<?php echo $cfg["cgi_base_url"];?>/tac.cgi" target="<?php echo $link_target;?>">Tactical Overview</a></li>
+			<li>
+				<a href="map.php?host=all" target="<?php echo $link_target;?>">Map</a>
+				<a href="<?php echo $cfg["cgi_base_url"];?>/statusmap.cgi?host=all" target="<?php echo $link_target;?>">(Legacy)</a>
+			</li>
+			<li><a href="<?php echo $cfg["cgi_base_url"];?>/status.cgi?hostgroup=all&amp;style=hostdetail" target="<?php echo $link_target;?>">Tickers</a></li>
+			<li><a href="<?php echo $cfg["cgi_base_url"];?>/status.cgi?host=all" target="<?php echo $link_target;?>">Indicators</a></li>
+			<li>
+				<a href="<?php echo $cfg["cgi_base_url"];?>/status.cgi?hostgroup=all&amp;style=overview" target="<?php echo $link_target;?>">Ticker Groups</a>
+				<ul>
+					<li><a href="<?php echo $cfg["cgi_base_url"];?>/status.cgi?hostgroup=all&amp;style=summary" target="<?php echo $link_target;?>">Summary</a></li>
+					<li><a href="<?php echo $cfg["cgi_base_url"];?>/status.cgi?hostgroup=all&amp;style=grid" target="<?php echo $link_target;?>">Grid</a></li>
+				</ul>
+			</li>
+			<li>
+				<a href="<?php echo $cfg["cgi_base_url"];?>/status.cgi?servicegroup=all&amp;style=overview" target="<?php echo $link_target;?>">Indicator Groups</a>
+				<ul>
+					<li><a href="<?php echo $cfg["cgi_base_url"];?>/status.cgi?servicegroup=all&amp;style=summary" target="<?php echo $link_target;?>">Summary</a></li>
+					<li><a href="<?php echo $cfg["cgi_base_url"];?>/status.cgi?servicegroup=all&amp;style=grid" target="<?php echo $link_target;?>">Grid</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+	<div class="navsectionheader">
+		<ul>
+			<li>Problems
+				<ul>
+					<li><a href="<?php echo $cfg["cgi_base_url"];?>/status.cgi?host=all&amp;servicestatustypes=28" target="<?php echo $link_target;?>">Indicators</a> (<a href="<?php echo $cfg["cgi_base_url"];?>/status.cgi?host=all&amp;type=detail&amp;hoststatustypes=3&amp;serviceprops=10&amp;servicestatustypes=28" target="<?php echo $link_target;?>">Unhandled</a>)</li>
+					<li><a href="<?php echo $cfg["cgi_base_url"];?>/status.cgi?hostgroup=all&amp;style=hostdetail&amp;hoststatustypes=12" target="<?php echo $link_target;?>">Tickers</a> (<a href="<?php echo $cfg["cgi_base_url"];?>/status.cgi?hostgroup=all&amp;style=hostdetail&amp;hoststatustypes=12&amp;hostprops=42" target="<?php echo $link_target;?>">Unhandled</a>)</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+	<div class="navbarsearch">
+		<form method="get" action="<?php echo $cfg["cgi_base_url"];?>/status.cgi" target="<?php echo $link_target;?>">
+			<fieldset>
+				<legend>Ticker Search:</legend>
+				<input type='hidden' name='navbarsearch' value='1'>
+				<input type='text' name='host' size='15' class="NavBarSearchItem">
+			</fieldset>
+		</form>
+	</div>
+</div>
+
+<div class="navsection">
+	<div class="navsectiontitle">Reports</div>
+	<div class="navsectionlinks">
+		<ul class="navsectionlinks">
+			<li><a href="<?php echo $cfg["cgi_base_url"];?>/avail.cgi" target="<?php echo $link_target;?>">Availability</a></li>
+			<li>
+				<a href="trends.html" target="<?php echo $link_target;?>">Trends</a>
+				<a href="<?php echo $cfg["cgi_base_url"];?>/trends.cgi" target="<?php echo $link_target;?>">(Legacy)</a>
+			</li>
+			<li><a href="<?php echo $cfg["cgi_base_url"];?>/history.cgi?host=all" target="<?php echo $link_target;?>">Alerts</a>
+			<ul>
+				<li><a href="<?php echo $cfg["cgi_base_url"];?>/history.cgi?host=all" target="<?php echo $link_target;?>">History</a></li>
+				<li><a href="<?php echo $cfg["cgi_base_url"];?>/summary.cgi" target="<?php echo $link_target;?>">Summary</a></li>
+				<li>
+					<a href="histogram.html" target="<?php echo $link_target;?>">Histogram</a>
+					<a href="<?php echo $cfg["cgi_base_url"];?>/histogram.cgi" target="<?php echo $link_target;?>">(Legacy)</a>
+				</li>
+			</ul>
+			</li>
+			<li><a href="<?php echo $cfg["cgi_base_url"];?>/notifications.cgi?contact=all" target="<?php echo $link_target;?>">Notifications</a></li>
+			<li><a href="<?php echo $cfg["cgi_base_url"];?>/showlog.cgi" target="<?php echo $link_target;?>">Event Log</a></li>
+		</ul>
+	</div>
+</div>
+
+<div class="navsection">
+	<div class="navsectiontitle">System</div>
+	<div class="navsectionlinks">
+		<ul class="navsectionlinks">
+			<li><a href="<?php echo $cfg["cgi_base_url"];?>/extinfo.cgi?type=3" target="<?php echo $link_target;?>">Comments</a></li>
+			<li><a href="<?php echo $cfg["cgi_base_url"];?>/extinfo.cgi?type=6" target="<?php echo $link_target;?>">Downtime</a></li>
+			<li><a href="<?php echo $cfg["cgi_base_url"];?>/extinfo.cgi?type=0" target="<?php echo $link_target;?>">Process Info</a></li>
+			<li><a href="<?php echo $cfg["cgi_base_url"];?>/extinfo.cgi?type=4" target="<?php echo $link_target;?>">Performance Info</a></li>
+			<li><a href="<?php echo $cfg["cgi_base_url"];?>/extinfo.cgi?type=7" target="<?php echo $link_target;?>">Scheduling Queue</a></li>
+			<li><a href="<?php echo $cfg["cgi_base_url"];?>/config.cgi" target="<?php echo $link_target;?>">Configuration</a></li>
+		</ul>
+	</div>
+</div>
+
+</body>
+</html>
