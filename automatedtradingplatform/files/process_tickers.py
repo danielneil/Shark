11d8e886349c3ca98e38947cc110269fafb83879
Daniel Neil <danielneilhodder@gmail.com>
2020-12-18 13:08:31 +1100

    added ticker list, and gernate script to stick them into nagios config

---
+++ automatedtradingplatform/files/process_tickers.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+# Builds the nagios configuration based on the ticker data.
+
+import csv
+
+with open ('ASX_Listed_Companies.csv', newline='') as csvfile:
+    tickerreader = csv.reader(csvfile, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+
+    # Skip the first row.
+    next(tickerreader)
+
+    for row in tickerreader:
+        ticker = row[0]
+
+
+        print( """
+            define host {
+                use linux-server
+                host_name """+ticker+"""
+                address 127.0.0.1
+                register 1
+            }
+
+        """)
