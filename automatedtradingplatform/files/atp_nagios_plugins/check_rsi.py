51b82f7a418ac6e17a06594c5bb949033b03c9df
Daniel Neil <danielneilhodder@gmail.com>
2021-01-02 20:05:20 +1100

    Update check_rsi.py

--- automatedtradingplatform/files/atp_nagios_plugins/check_rsi.py
+++ automatedtradingplatform/files/atp_nagios_plugins/check_rsi.py
@@ -6,6 +6,30 @@ import datetime
 
 import sys
 
+def computeRSI (data, time_window):
+    diff = data.diff(1).dropna()        # diff in one field(one day)
+
+    #this preservers dimensions off diff values
+    up_chg = 0 * diff
+    down_chg = 0 * diff
+    
+    # up change is equal to the positive difference, otherwise equal to zero
+    up_chg[diff > 0] = diff[ diff>0 ]
+    
+    # down change is equal to negative deifference, otherwise equal to zero
+    down_chg[diff < 0] = diff[ diff < 0 ]
+    
+    # check pandas documentation for ewm
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.ewm.html
+    # values are related to exponential decay
+    # we set com=time_window-1 so we get decay alpha=1/time_window
+    up_chg_avg   = up_chg.ewm(com=time_window-1 , min_periods=time_window).mean()
+    down_chg_avg = down_chg.ewm(com=time_window-1 , min_periods=time_window).mean()
+    
+    rs = abs(up_chg_avg/down_chg_avg)
+    rsi = 100 - 100/(1+rs)
+    return rsi
+
 if __name__ == "__main__":
 
     ticker = sys.argv[1]
@@ -13,20 +37,6 @@ if __name__ == "__main__":
 
     data = pd.read_csv('/atp/ticker-data/'+ticker+'.AX.txt')
 
-    dataFrame = data['Adj Close']
-
-    lastPrice = np.round(dataFrame.iloc[-1], 2)
-    yesterdayPrice = np.round(dataFrame.iloc[-2], 2)
-
-    if lastPrice > yesterdayPrice:
-        percentageDiff = '{0:.2f}'.format(abs((yesterdayPrice/lastPrice * 100) -100)) 
-
-        print("$" + str(lastPrice) + " (" + str(percentageDiff) + "%)")
-        sys.exit(0)
-    else:
-        percentageDiff = '{0:.2f}'.format((lastPrice/yesterdayPrice * 100) -100)
-
-        print("$" + str(lastPrice) + " (" + str(percentageDiff) + "%)")
-        sys.exit(2)
-
-
+    dataFrame['RSI'] = computeRSI(data['Adj Close'], rsiPeriod)
+    
+    print(df.tail())
