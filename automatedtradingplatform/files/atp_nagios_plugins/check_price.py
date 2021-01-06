ed79ee18e3dd9cbf924a35d4818c61cf123ce18f
danielneil <you@example.com>
2021-01-06 13:32:45 +1100

    added a raw arg which will just print the last price and return ok

--- automatedtradingplatform/files/atp_nagios_plugins/check_price.py
+++ automatedtradingplatform/files/atp_nagios_plugins/check_price.py
@@ -12,12 +12,14 @@ WARNING      = 1
 CRITICAL     = 2
 UNKNOWN      = 3
 
-cmd_arg_help = "This plugin computes the relative strength index (RSI) for a stock."
+cmd_arg_help = "Gets the latest price of a stock."
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description=cmd_arg_help)
-    parser.add_argument("-t", "--ticker", help="ticker code of the stock")
+    parser.add_argument("-t", "--ticker", help="Ticker code of the stock")
+    parser.add_argument("-r", "--raw", nargs='?', const=1, type=int, help="Just print the latest price. Will always return OK(0)")
+
     args = parser.parse_args()
 
 
@@ -31,21 +33,24 @@ if __name__ == "__main__":
             sys.exit(UNKNOWN)
 
     ticker = args.ticker
-
     data = pd.read_csv('/atp/ticker-data/'+ticker+'.AX.txt')
-
     dataFrame = data['Adj Close']
 
     lastPrice = np.round(dataFrame.iloc[-1], 2)
     yesterdayPrice = np.round(dataFrame.iloc[-2], 2)
 
+    if args.raw:
+        print(lastPrice)
+        sys.exit(OK)
+
+
     if lastPrice > yesterdayPrice:
         percentageDiff = '{0:.2f}'.format(abs((yesterdayPrice/lastPrice * 100) -100)) 
 
         print("$" + str(lastPrice) + " (" + str(percentageDiff) + "%)")
-        sys.exit(0)
+        sys.exit(OK)
     else:
         percentageDiff = '{0:.2f}'.format((lastPrice/yesterdayPrice * 100) -100)
 
         print("$" + str(lastPrice) + " (" + str(percentageDiff) + "%)")
-        sys.exit(2)
+        sys.exit(CRITICAL)
