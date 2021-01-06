14ed992d5da9478fb9f858fb254f909185f1cf8c
danielneil <you@example.com>
2021-01-06 11:56:10 +1100

    refactored code based on template

--- automatedtradingplatform/files/atp_nagios_plugins/check_price.py
+++ automatedtradingplatform/files/atp_nagios_plugins/check_price.py
@@ -3,12 +3,34 @@
 import pandas as pd
 import numpy as np
 import datetime
-
+import argparse
 import sys
+import os
+
+OK           = 0
+WARNING      = 1
+CRITICAL     = 2
+UNKNOWN      = 3
+
+cmd_arg_help = "This plugin computes the relative strength index (RSI) for a stock."
 
 if __name__ == "__main__":
 
-    ticker = sys.argv[1]
+    parser = argparse.ArgumentParser(description=cmd_arg_help)
+    parser.add_argument("-t", "--ticker", help="ticker code of the stock")
+    args = parser.parse_args()
+
+
+    if not args.ticker:
+        print ("UNKNOWN - No ticker found")
+        sys.exit(UNKNOWN)
+
+    else:
+        if not os.path.isfile('/atp/ticker-data/'+args.ticker+'.AX.txt'):
+            print ("UNKNOWN - ticker data file not found")
+            sys.exit(UNKNOWN)
+
+    ticker = args.ticker
 
     data = pd.read_csv('/atp/ticker-data/'+ticker+'.AX.txt')
 
@@ -27,5 +49,3 @@ if __name__ == "__main__":
 
         print("$" + str(lastPrice) + " (" + str(percentageDiff) + "%)")
         sys.exit(2)
-
-
