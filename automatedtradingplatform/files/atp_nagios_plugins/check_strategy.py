0dd84232947db57ca3599d3f4e98a079d96859ef
danielneil <you@example.com>
2021-01-11 23:37:14 +1100

    started templating out the check_strategy

--- automatedtradingplatform/files/atp_nagios_plugins/check_strategy.py
+++ automatedtradingplatform/files/atp_nagios_plugins/check_strategy.py
@@ -12,43 +12,25 @@ WARNING      = 1
 CRITICAL     = 2
 UNKNOWN      = 3
 
-cmd_arg_help = "This strategy is simple moving averages, buy when the first moving average, crosses above the second. Opposite for shorts."
+cmd_arg_help = "This script calls the strategy the strategy defined in the nagios configuration. Do not make changes here. See the template strategy for an example"
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description=cmd_arg_help)
     
-    parser.add_argument("-t", "--ticker", help="Ticker code of the stock")
-    parser.add_argument("-f", "--firstMA", help="First moving average (the shorter, e.g. 10 days)")
-    parser.add_argument("-s", "--secondMA", help="Second moving average (the longer, e.g. 50 days)")
-    
+    parser.add_argument("-t", "--ticker", help="Ticker code of the stock in question")
+    parser.add_argument("-s", "--strategy", help="The file name of the custom strategy")
     args = parser.parse_args()
 
     if not args.ticker:
-        print ("UNKNOWN - No ticker found")
+        print ("UNKNOWN - No ticker specified")
         sys.exit(UNKNOWN)
 
-    if not args.firstMA:
-        print ("UNKNOWN - No first moving average found")
+    if not args.strategy:
+        print("UNKNOWN - No strategy file specified")
         sys.exit(UNKNOWN)
 
-    if not args.secondMA:
-        print ("UNKNOWN - No second moving average found")
-        sys.exit(UNKNOWN)
-  
     ticker = args.ticker 
-    firstMA = args.firstMA
-    secondMA = args.secondMA
+    strategyFile = args.strategy
 
-    firstSMAPeriod = subprocess.check_output(['/atp/nagios_plugins/check_sma.py', ticker, firstMA])
-    secondSMAPeriod = subprocess.check_output(['/atp/nagios_plugins/check_sma.py', ticker, secondMA])
-
-    if firstSMAPeriod > secondSMAPeriod:
-
-            print("Long Opportunity")
-            sys.exit(WARNING)
-    else:
-            print("Short Opportunity")
-            sys.exit(WARNING)
-
-    sys.exit(0)
+    subprocess.check_output(['/atp/strategies/' + strategyFile + ' --ticker', ticker])
