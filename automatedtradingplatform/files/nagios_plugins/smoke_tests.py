e50a5a4e0b7320613b7ddbf6a615500a7e911fd9
danielneil <you@example.com>
2021-01-12 13:57:58 +1100

    smoke tests now have coverage of check_price.py

--- automatedtradingplatform/files/nagios_plugins/smoke_tests.py
+++ automatedtradingplatform/files/nagios_plugins/smoke_tests.py
@@ -13,7 +13,8 @@ TICKER="CBA"
 EXPECTED_RSI=62.95
 
 print("########################################################################")
-print("# Check RSI Plugin\n")
+print("# Check RSI")
+print("########################################################################")
 
 # Check computed RSI period is correct. 
 
@@ -75,5 +76,64 @@ except subprocess.CalledProcessError as e:
 		print("OK - RSI min was triggered and threw an error.")
 	else:
 		print("ERROR - RSI min was not triggered and did not throw an error, exiting...")
+		print(e.output)
+		sys.exit(ERROR)
+
+########################################################################
+# Check Price 
+
+TICKER="CBA"
+EXPECTED_PRICE=83.16
+PRETTY_PRICE="$83.16 (-1.84%)"
+
+print("########################################################################")
+print("# Check Price Plugin")
+print("########################################################################")
+
+# Check raw price is correct. 
+
+priceCheck = float(subprocess.check_output(["/atp/nagios_plugins/check_price.py", "--ticker", TICKER, "--raw"]))
+
+if priceCheck == EXPECTED_PRICE:
+	print("OK - Price check of " + str(EXPECTED_PRICE) + " correct for ticker " + TICKER)
+else:
+	print("ERROR - price check of " + str(EXPECTED_PRICE) + " incorrect for ticker " + TICKER + ", exiting...")
+	sys.exit(ERROR)
+
+# No ticker entered.
+
+try:
+
+	subprocess.check_output(["/atp/nagios_plugins/check_price.py"])
+	print("Error, a ticker was not supplied, and therefore should have thrown an error, exiting...")
+	sys.exit(ERROR)
+
+except subprocess.CalledProcessError as e:
+
+	if "UNKNOWN - No ticker found" == e.output.rstrip():
+		print("OK - a ticker was not supplied and threw an error.")
+	else:
+
+		print("ERROR - a ticker was not supplied and something went wrong, exiting...")
+		print(e.output)
+		sys.exit(ERROR)
+
+# Check pretty price is correct. 
+
+# No ticker entered.
+
+try:
+
+	subprocess.check_output(["/atp/nagios_plugins/check_price.py", "--ticker", "CBA"])
+	print("Pretty price did not match expected result, and therefore should have thrown an error, exiting...")
+	sys.exit(ERROR)
+
+except subprocess.CalledProcessError as e:
+
+	if PRETTY_PRICE == e.output.rstrip():
+		print("OK - Pretty price matched expected result: " + "$83.16 (-1.84%)")
+	else:
+
+		print("ERROR - Pretty price did not match expected result (" + "$83.16 (-1.84%)" + " ) and something went wrong, exiting...")
 		print(e.output)
 		sys.exit(ERROR)
