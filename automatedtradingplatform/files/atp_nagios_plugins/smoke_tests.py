9f08282925fb4bd8f2ed22614ac8d5ffe6616e37
danielneil <you@example.com>
2021-01-12 11:46:32 +1100

    complete coverage of check_rsi.py

--- automatedtradingplatform/files/atp_nagios_plugins/smoke_tests.py
+++ automatedtradingplatform/files/atp_nagios_plugins/smoke_tests.py
@@ -8,18 +8,74 @@ OK= 0
 
 TICKER="CBA"
 
-########################
+########################################################################
 # Check RSI Plugin
 
 TICKER="CBA"
 EXPECTED_RSI=62.95
 
-print("Running check_rsi.py checks....")
+print("########################################################################")
+print("# Check RSI Plugin\n")
+
+# Check computed RSI period is correct. 
 
 rsiCheck = float(subprocess.check_output(["/atp/nagios_plugins/check_rsi.py", "--ticker", TICKER, "--rsiPeriod", "14"]))
 
 if rsiCheck == EXPECTED_RSI:
-	print("RSI Calculation of " + EXPECTED_RSI + " correct for ticker " + TICKER)
+	print("OK - RSI Calculation of " + str(EXPECTED_RSI) + " correct for ticker " + TICKER)
 else:
-	print("RSI Calculation of " + EXPECTED_RSI + " incorrect for ticker " + TICKER)
+	print("ERROR - RSI Calculation of " + str(EXPECTED_RSI) + " incorrect for ticker " + TICKER + ", exiting...")
 	sys.exit(ERROR)
+
+# No ticker entered.
+
+try:
+
+	rsiCheck = float(subprocess.check_output(["/atp/nagios_plugins/check_rsi.py"]))
+	print("Error, a ticker was not supplied, and therefore should have thrown an error, exiting...")
+	sys.exit(ERROR)
+
+except subprocess.CalledProcessError as e:
+
+	if "UNKNOWN - No ticker found" == e.output.rstrip():
+		print("OK - a ticker was not supplied and threw an error.")
+	else:
+
+		print("ERROR - a ticker was not supplied and something went wrong, exiting...")
+		print(e.output)
+		sys.exit(ERROR)
+
+# Max RSI period.
+
+try:
+
+	rsiMaxCheck = subprocess.check_output(["/atp/nagios_plugins/check_rsi.py", "--ticker", TICKER, "--rsiPeriod", "14", "--max", "50"])
+	print("Error, RSI is greater than max, and therefore should have thrown an error, exiting...")
+	sys.exit(ERROR)
+
+except subprocess.CalledProcessError as e:
+
+	if "WARNING - RSI(62.95) is above 50" == e.output.rstrip():
+		print("OK - RSI max was triggered and threw an error.")
+	else:
+
+		print("ERROR - RSI max was not triggered and did not throw an error, exiting...")
+		print(e.output)
+		sys.exit(ERROR)
+
+# Min RSI period.
+
+try:
+
+	rsiMaxCheck = subprocess.check_output(["/atp/nagios_plugins/check_rsi.py", "--ticker", TICKER, "--rsiPeriod", "14", "--min", "70"])
+	print("Error, RSI is less than min, and therefore should have thrown an error, exiting...")
+	sys.exit(ERROR)
+
+except subprocess.CalledProcessError as e:
+
+	if "WARNING - RSI(62.95) is below 70" == e.output.rstrip():
+		print("OK - RSI min was triggered and threw an error.")
+	else:
+		print("ERROR - RSI min was not triggered and did not throw an error, exiting...")
+		print(e.output)
+		sys.exit(ERROR)
