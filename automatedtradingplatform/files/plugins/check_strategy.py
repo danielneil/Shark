d92bd709bb8005aea5acd8233f72835b957156ed
danielneil <you@example.com>
2021-01-14 12:22:07 +1100

    fixed doc

--- automatedtradingplatform/files/plugins/check_strategy.py
+++ automatedtradingplatform/files/plugins/check_strategy.py
@@ -11,7 +11,7 @@ OK           = 0
 WARNING      = 1
 CRITICAL     = 2
 UNKNOWN      = 3
-cmd_arg_help = "This script calls the strategy the strategy defined in the nagios configuration. Do not make changes here. See the [template strategy](AutomatedTradingPlatform/blob/main/automatedtradingplatform/files/strategies/moving_averages.py) for an example"
+cmd_arg_help = "This script calls the strategy the strategy defined in the nagios configuration. Do not make changes here. See the [template strategy](https://github.com/danielneil/AutomatedTradingPlatform/blob/main/automatedtradingplatform/files/strategies/moving_averages.py) for an example"
 
 if __name__ == "__main__":
 
