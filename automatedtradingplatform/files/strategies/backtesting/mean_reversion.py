fbf68f7fcb2ce5731756d936a9c4e8500fb00b51
Daniel Neil <danielneilhodder@gmail.com>
2021-01-26 15:18:58 +1100

    Create mean_reversion.py

---
+++ automatedtradingplatform/files/strategies/backtesting/mean_reversion.py
@@ -0,0 +1,110 @@
+from pyalgotrade import strategy
+from pyalgotrade.technical import ma
+from pyalgotrade.technical import rsi
+from pyalgotrade.technical import cross
+
+
+class RSI2(strategy.BacktestingStrategy):
+    def __init__(self, feed, instrument, entrySMA, exitSMA, rsiPeriod, overBoughtThreshold, overSoldThreshold):
+        super(RSI2, self).__init__(feed)
+        self.__instrument = instrument
+        # We'll use adjusted close values, if available, instead of regular close values.
+        if feed.barsHaveAdjClose():
+            self.setUseAdjustedValues(True)
+        self.__priceDS = feed[instrument].getPriceDataSeries()
+        self.__entrySMA = ma.SMA(self.__priceDS, entrySMA)
+        self.__exitSMA = ma.SMA(self.__priceDS, exitSMA)
+        self.__rsi = rsi.RSI(self.__priceDS, rsiPeriod)
+        self.__overBoughtThreshold = overBoughtThreshold
+        self.__overSoldThreshold = overSoldThreshold
+        self.__longPos = None
+        self.__shortPos = None
+
+    def getEntrySMA(self):
+        return self.__entrySMA
+
+    def getExitSMA(self):
+        return self.__exitSMA
+
+    def getRSI(self):
+        return self.__rsi
+
+    def onEnterCanceled(self, position):
+        if self.__longPos == position:
+            self.__longPos = None
+        elif self.__shortPos == position:
+            self.__shortPos = None
+        else:
+            assert(False)
+
+    def onExitOk(self, position):
+        if self.__longPos == position:
+            self.__longPos = None
+        elif self.__shortPos == position:
+            self.__shortPos = None
+        else:
+            assert(False)
+
+    def onExitCanceled(self, position):
+        # If the exit was canceled, re-submit it.
+        position.exitMarket()
+
+    def onBars(self, bars):
+        # Wait for enough bars to be available to calculate SMA and RSI.
+        if self.__exitSMA[-1] is None or self.__entrySMA[-1] is None or self.__rsi[-1] is None:
+            return
+
+        bar = bars[self.__instrument]
+        if self.__longPos is not None:
+            if self.exitLongSignal():
+                self.__longPos.exitMarket()
+        elif self.__shortPos is not None:
+            if self.exitShortSignal():
+                self.__shortPos.exitMarket()
+        else:
+            if self.enterLongSignal(bar):
+                shares = int(self.getBroker().getCash() * 0.9 / bars[self.__instrument].getPrice())
+                self.__longPos = self.enterLong(self.__instrument, shares, True)
+            elif self.enterShortSignal(bar):
+                shares = int(self.getBroker().getCash() * 0.9 / bars[self.__instrument].getPrice())
+                self.__shortPos = self.enterShort(self.__instrument, shares, True)
+
+    def enterLongSignal(self, bar):
+        return bar.getPrice() > self.__entrySMA[-1] and self.__rsi[-1] <= self.__overSoldThreshold
+
+    def exitLongSignal(self):
+        return cross.cross_above(self.__priceDS, self.__exitSMA) and not self.__longPos.exitActive()
+
+    def enterShortSignal(self, bar):
+        return bar.getPrice() < self.__entrySMA[-1] and self.__rsi[-1] >= self.__overBoughtThreshold
+
+    def exitShortSignal(self):
+        return cross.cross_below(self.__priceDS, self.__exitSMA) and not self.__shortPos.exitActive()
+        
+        
+        
+import itertools
+from pyalgotrade.optimizer import server
+from pyalgotrade.barfeed import quandlfeed
+
+
+def parameters_generator():
+    instrument = ["ibm"]
+    entrySMA = range(150, 251)
+    exitSMA = range(5, 16)
+    rsiPeriod = range(2, 11)
+    overBoughtThreshold = range(75, 96)
+    overSoldThreshold = range(5, 26)
+    return itertools.product(instrument, entrySMA, exitSMA, rsiPeriod, overBoughtThreshold, overSoldThreshold)
+
+
+# The if __name__ == '__main__' part is necessary if running on Windows.
+if __name__ == '__main__':
+    # Load the bar feed from the CSV files.
+    feed = quandlfeed.Feed()
+    feed.addBarsFromCSV("ibm", "WIKI-IBM-2009-quandl.csv")
+    feed.addBarsFromCSV("ibm", "WIKI-IBM-2010-quandl.csv")
+    feed.addBarsFromCSV("ibm", "WIKI-IBM-2011-quandl.csv")
+
+    # Run the server.
+    server.serve(feed, parameters_generator(), "localhost", 5000)
