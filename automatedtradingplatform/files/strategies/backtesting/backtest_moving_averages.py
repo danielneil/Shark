8b3c88cc4c15d7eda0640619c448e588d24cf2b6
Daniel Neil <danielneilhodder@gmail.com>
2021-02-21 13:45:26 +1100

    Update backtest_moving_averages.py

--- automatedtradingplatform/files/strategies/backtesting/backtest_moving_averages.py
+++ automatedtradingplatform/files/strategies/backtesting/backtest_moving_averages.py
@@ -70,25 +70,90 @@ def run_strategy(ticker, shares, capital, longsma, shortsma):
     feed.addBarsFromCSV(ticker, "/shark/ticker-data/"+ticker+".AX.txt")
 
     # Evaluate the strategy with the feed.
-    maStrategy = MovingAverages(feed, ticker, shares, capital, longsma, shortsma)
+    strat = MovingAverages(feed, ticker, shares, capital, longsma, shortsma)
     
+    # Attach  analyzers to the strategy before executing it.
     sharpeRatioAnalyzer = sharpe.SharpeRatio()
-    maStrategy.attachAnalyzer(sharpeRatioAnalyzer)
+    strat.attachAnalyzer(sharpeRatioAnalyzer)
     
-    maStrategy.run()
+    retAnalyzer = returns.Returns()
+    strat.attachAnalyzer(retAnalyzer)
     
-    # print("Final portfolio value: $%.2f" % maStrategy.getBroker().getEquity())
+    drawDownAnalyzer = drawdown.DrawDown()
+    strat.attachAnalyzer(drawDownAnalyzer)
     
-    print("Sharpe Ratio: %.2f" % sharpeRatioAnalyzer.getSharpeRatio(0.05))
-    sharpeRatio = sharpeRatioAnalyzer.getSharpeRatio(0.05)
+    tradesAnalyzer = trades.Trades()
+    strat.attachAnalyzer(tradesAnalyzer)
+
+    strat.run()
+    
+    
+    
+    
+    print("Final portfolio value: $%.2f" % myStrategy.getResult())
+    print("Cumulative returns: %.2f %%" % (retAnalyzer.getCumulativeReturns()[-1] * 100))
+    print("Sharpe ratio: %.2f" % (sharpeRatioAnalyzer.getSharpeRatio(0.05)))
+    print("Max. drawdown: %.2f %%" % (drawDownAnalyzer.getMaxDrawDown() * 100))
+    print("Longest drawdown duration: %s" % (drawDownAnalyzer.getLongestDrawDownDuration()))
+
+    print("")
+    print("Total trades: %d" % (tradesAnalyzer.getCount()))
+
+    if tradesAnalyzer.getCount() > 0:
+      profits = tradesAnalyzer.getAll()
+      print("Avg. profit: $%2.f" % (profits.mean()))
+      print("Profits std. dev.: $%2.f" % (profits.std()))
+      print("Max. profit: $%2.f" % (profits.max()))
+      print("Min. profit: $%2.f" % (profits.min()))
+      returns = tradesAnalyzer.getAllReturns()
+      print("Avg. return: %2.f %%" % (returns.mean() * 100))
+      print("Returns std. dev.: %2.f %%" % (returns.std() * 100))
+      print("Max. return: %2.f %%" % (returns.max() * 100))
+      print("Min. return: %2.f %%" % (returns.min() * 100)) 
+
+      print("")
+      print("Profitable trades: %d" % (tradesAnalyzer.getProfitableCount()))
+
+   if tradesAnalyzer.getProfitableCount() > 0:
+      profits = tradesAnalyzer.getProfits()
+      print("Avg. profit: $%2.f" % (profits.mean()))
+      print("Profits std. dev.: $%2.f" % (profits.std()))
+      print("Max. profit: $%2.f" % (profits.max()))
+      print("Min. profit: $%2.f" % (profits.min()))
+      returns = tradesAnalyzer.getPositiveReturns()
+      print("Avg. return: %2.f %%" % (returns.mean() * 100))
+      print("Returns std. dev.: %2.f %%" % (returns.std() * 100))
+      print("Max. return: %2.f %%" % (returns.max() * 100))
+      print("Min. return: %2.f %%" % (returns.min() * 100))
+
+      print("")
+      print("Unprofitable trades: %d" % (tradesAnalyzer.getUnprofitableCount()))
+
+   if tradesAnalyzer.getUnprofitableCount() > 0:
+     losses = tradesAnalyzer.getLosses()
+     print("Avg. loss: $%2.f" % (losses.mean()))
+     print("Losses std. dev.: $%2.f" % (losses.std()))
+     print("Max. loss: $%2.f" % (losses.min()))
+     print("Min. loss: $%2.f" % (losses.max()))
+     returns = tradesAnalyzer.getNegativeReturns()
+     print("Avg. return: %2.f %%" % (returns.mean() * 100))
+     print("Returns std. dev.: %2.f %%" % (returns.std() * 100))
+     print("Max. return: %2.f %%" % (returns.max() * 100))
+     print("Min. return: %2.f %%" % (returns.min() * 100))
+    
+    
+    
+    
+   print("Sharpe Ratio: %.2f" % sharpeRatioAnalyzer.getSharpeRatio(0.05))
+   sharpeRatio = sharpeRatioAnalyzer.getSharpeRatio(0.05)
 
     # Write detailed backtest file to the web dir
 
 
-    if sharpeRatio > 0: 
-        sys.exit(OK)
-    else:
-        sys.exit(CRITICAL)
+   if sharpeRatio > 0: 
+       sys.exit(OK)
+   else:
+       sys.exit(CRITICAL)
 
 
 if __name__ == "__main__":
