7714a3be2fbf354ab3ea793402d29d0430a9fe97
Daniel Neil <danielneilhodder@gmail.com>
2021-02-25 16:52:03 +1100

    Update smoke_test.py

--- apache-drill/files/smoke_test.py
+++ apache-drill/files/smoke_test.py
@@ -7,12 +7,12 @@ drill = PyDrill(host='localhost', port=8047)
 if not drill.is_active():
     raise ImproperlyConfigured('Please run Drill first')
 
-yelp_reviews = drill.query('''
+test_json = drill.query('''
   SELECT * FROM
   `dfs.root`.`/home/daniel/Shark/apache-drill/files/testFile.json`
   LIMIT 1
 ''')
 
-for result in yelp_reviews:
+for result in test_json:
     print("%s: %s" %(result['ticker'], result['quantity']))
 
