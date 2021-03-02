9973fd0b47dfe5998670ed99915c84a43279d3d4
Daniel Neil <danielneilhodder@gmail.com>
2021-03-02 11:35:45 +1100

    Update ui_backtest_refresh.py

--- algorithmictradingplatform/files/ui_refresh/ui_backtest_refresh.py
+++ algorithmictradingplatform/files/ui_refresh/ui_backtest_refresh.py
@@ -1,18 +1,42 @@
 #!/usr/bin/python3
 
+import datetime
+import sys
+import subprocess
+import argparse
+
 from pydrill.client import PyDrill
+
+OK           = 0
+WARNING      = 1
+CRITICAL     = 2
+UNKNOWN      = 3
 
 drill = PyDrill(host='localhost', port=8047)
 
 if not drill.is_active():
     raise ImproperlyConfigured('Please run Drill first')
 
-tradeCount = drill.query('''
-  SELECT count(datetime) FROM
-  dfs.tradelog.`SCG.trade.log`
-''')
+cmd_arg_help = "Refreshes the backtest UI stuff for a specific ticker"
 
-df = tradeCount.to_dataframe()
-count = df.iloc[0,0]
+if __name__ == "__main__":
 
-print(count)
+    parser = argparse.ArgumentParser(description=cmd_arg_help)
+    parser.add_argument("-t", "--ticker", help="Ticker of the stock in question.")
+    args = parser.parse_args()
+
+    if not args.ticker:
+        print ("UNKNOWN - No ticker specified")
+        sys.exit(UNKNOWN)
+    
+    ticker = args.ticker
+    
+    tradeCount = drill.query('''
+     SELECT count(datetime) FROM
+     dfs.tradelog.`SCG.trade.log`
+    ''')
+
+    df = tradeCount.to_dataframe()
+    count = df.iloc[0,0]
+
+    print(count)
