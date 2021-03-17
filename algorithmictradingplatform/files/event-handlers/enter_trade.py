65822c9bf0adc8f94ad92e866fd131e069fd00cd
Daniel Neil <danielneilhodder@gmail.com>
2021-03-17 22:07:45 +1100

    Update enter_trade.py

--- algorithmictradingplatform/files/event-handlers/enter_trade.py
+++ algorithmictradingplatform/files/event-handlers/enter_trade.py
@@ -16,7 +16,11 @@ cmd_arg_help = "Send a BUY order to the queue in RabbitMQ"
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description=cmd_arg_help)
-    parser.add_argument("-t", "--ticker", help="Ticker code of the stock.")
+    parser.add_argument("-t", "--ticker", help="Ticker code of the stock.")    
+    parser.add_argument("-ss", "--servicestate", help="OK/WARNING/UNKNOWN/CRITICAL") 
+    parser.add_argument("-st", "--statetype", help="SOFT/HARD") 
+    parser.add_argument("-sa", "--serviceattempt", help="1/2/3")
+    
     args = parser.parse_args()
 
     if not args.ticker:
@@ -25,6 +29,13 @@ if __name__ == "__main__":
 
     ticker = args.ticker
 
+    if not args.servicestate == "CRITICAL":
+        sys.exit(exitcode)
+        
+    if not args.statetype == "HARD":    
+        if args.statetype == "SOFT" and if not args.serviceattempt == 3:
+            sys.exit(exitcode)
+    
     with pika.BlockingConnection(pika.ConnectionParameters('localhost')) as connection:
 
         channel = connection.channel()
