b320b83a16028aadc4d0d0db57311dba83ad1bf1
danielneil <you@example.com>
2021-03-17 21:52:12 +1100

    added a very rudimentary BUY script which places buy orders

--- algorithmictradingplatform/files/event-handlers/enter_trade.py
+++ algorithmictradingplatform/files/event-handlers/enter_trade.py
@@ -11,21 +11,21 @@ WARNING      = 1
 CRITICAL     = 2
 UNKNOWN      = 3
 
-cmd_arg_help = "Send a BUY order to the Queue in Rabbit Exchange"
+cmd_arg_help = "Send a BUY order to the queue in RabbitMQ"
 
 if __name__ == "__main__":
 
-    	parser = argparse.ArgumentParser(description=cmd_arg_help)
-    	parser.add_argument("-t", "--ticker", help="Ticker code of the stock.")
-    	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description=cmd_arg_help)
+    parser.add_argument("-t", "--ticker", help="Ticker code of the stock.")
+    args = parser.parse_args()
 
-    	if not args.ticker:
-        	print ("UNKNOWN - No ticker found")
-        	sys.exit(UNKNOWN)
+    if not args.ticker:
+        print ("UNKNOWN - No ticker found")
+        sys.exit(UNKNOWN)
 
-	ticker = args.ticker
-		
-	with pika.BlockingConnection(pika.ConnectionParameters('localhost')) as connection:
-		
-		channel = connection.channel()
-		channel.basic_publish(exchange='', routing_key='BUYS', body="TICKER:" + ticker)
+    ticker = args.ticker
+
+    with pika.BlockingConnection(pika.ConnectionParameters('localhost')) as connection:
+
+        channel = connection.channel()
+        channel.basic_publish(exchange='', routing_key='BUYS', body="TICKER:" + ticker)
