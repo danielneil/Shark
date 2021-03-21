c8692896abce1821ebb0a6f3b26589a861d8a6c1
Daniel Neil <danielneilhodder@gmail.com>
2021-03-21 11:03:59 +1100

    Update main.php

--- algorithmictradingplatform/files/ui_patches/main.php
+++ algorithmictradingplatform/files/ui_patches/main.php
@@ -151,7 +151,7 @@ $this_year = '2020';
 		<div id="splashbox1" class="splashbox splashbox-clear">
 			<h2>Trading Overview</h2>
 			<ul>
-				<li><a href="https://go.nagios.com/nagioscore/startmonitoring" target="_blank">Start monitoring your infrastructure</a></li>
+				<li><a href="https://go.nagios.com/nagioscore/startmonitoring" target="_blank"></a></li>
 				<li><a href="https://go.nagios.com/nagioscore/changelook" target="_blank">Change the look and feel of Nagios</a></li>
 				<li><a href="https://go.nagios.com/nagioscore/extend" target="_blank">Extend Nagios with hundreds of addons</a></li>
 				<!--<li><a href="https://go.nagios.com/nagioscore/docs" target="_blank">Read the Nagios documentation</a></li>-->
@@ -172,8 +172,8 @@ $this_year = '2020';
 				<li><a href="https://www.nagios.org" target="_blank">Nagios.org</a> (project)</li>
 			</ul>
 		</div>
-		<div id="splashbox3" class="splashbox">
-			<h2>Top Opportunities</h2>
+		<div id="splashbox1" class="splashbox">
+			<h2>Indicators</h2>
 			<ul>
 				<li><a href="https://library.nagios.com" target="_blank">Nagios Library</a> (tutorials and docs)</li>
 				<li><a href="https://labs.nagios.com" target="_blank">Nagios Labs</a> (development blog)</li>
@@ -189,7 +189,7 @@ $this_year = '2020';
 	<div id="splashrow2">
 
 		<div id="splashbox4" class="splashbox splashbox-clear"><!-- latest news feed -->
-			<h2>Backtest Results</h2>
+			<h2>Backtesting Overview</h2>
 			<div id="splashbox4-contents">
 			</div>
 		</div>
