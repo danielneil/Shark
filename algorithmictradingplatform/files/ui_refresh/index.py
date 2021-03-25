488e651f2d0cfa5898646be0fd003120575ed57f
Daniel Neil <danielneilhodder@gmail.com>
2021-03-25 20:10:06 +1100

    Update index.py

--- algorithmictradingplatform/files/ui_refresh/index.py
+++ algorithmictradingplatform/files/ui_refresh/index.py
@@ -1,3 +1,7 @@
-# The index file for the transaction page
-
-
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-# enable debugging
+import cgitb
+    
+cgitb.enable()    print(“Content-Type: text/html;charset=utf-8”)
+    
+print()    print(“Hello World!”)
