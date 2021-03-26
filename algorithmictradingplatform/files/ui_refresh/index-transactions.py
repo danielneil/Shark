6849de052d90b3042cac5f26fbaed8dd9d0bad8c
Daniel Neil <danielneilhodder@gmail.com>
2021-03-27 00:11:39 +1100

    Update index-transactions.py

--- algorithmictradingplatform/files/ui_refresh/index-transactions.py
+++ algorithmictradingplatform/files/ui_refresh/index-transactions.py
@@ -4,6 +4,7 @@
 
 import cgitb
 import pandas
+import sys 
 from pydrill.client import PyDrill
 
 #####################################
@@ -21,11 +22,9 @@ drill = PyDrill(host='localhost', port=8047)
 
 if not drill.is_active():
    print("Please run Drill first")
+   sys.exit(1)
 
 # Select all the transactions with SQL, and we'll manipulate with the pandas functions.
 transactions = drill.query("select * from `dfs.backtests`.`transactions`")
-
 dataframe = transactions.to_dataframe()
-
-
 
