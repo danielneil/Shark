4d854997bf38c50a8356042751cd2c0f41c483dc
Daniel Neil <danielneilhodder@gmail.com>
2021-03-26 20:11:10 +1100

    Update side.php

--- algorithmictradingplatform/files/ui_patches/side.php
+++ algorithmictradingplatform/files/ui_patches/side.php
@@ -68,10 +68,10 @@ $link_target = 'main';
 	<div class="navsectiontitle">Backtesting</div>
 	<div class="navsectionlinks">
 		<ul class="navsectionlinks">
-			<li><a href="/shark/backtest" target="<?php echo $link_target;?>">Summary</a>
+			<li><a href="/shark/backtest/index.py" target="<?php echo $link_target;?>">Summary</a>
 			<ul>
-				<li><a href="/shark/backtest/reports" target="<?php echo $link_target;?>">Reports</a></li>
-				<li><a href="/shark/backtest/transactions" target="<?php echo $link_target;?>">Transactions</a></li>
+				<li><a href="/shark/backtest/reports/index.py" target="<?php echo $link_target;?>">Reports</a></li>
+				<li><a href="/shark/backtest/transactions/index.py" target="<?php echo $link_target;?>">Transactions</a></li>
 			</ul>
 			</li>
 		</ul>
