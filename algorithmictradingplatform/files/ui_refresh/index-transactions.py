a83232c59d5e0de98188d8afe9b65856cff1c21a
Daniel Neil <danielneilhodder@gmail.com>
2021-03-27 00:05:25 +1100

    Update index-transactions.py

--- algorithmictradingplatform/files/ui_refresh/index-transactions.py
+++ algorithmictradingplatform/files/ui_refresh/index-transactions.py
@@ -1,34 +1,19 @@
 #!/usr/bin/python3
 
+# Summarises the transactions into a single web page.
+
 import cgitb
-
-# Using jinja templating engine, it gernates the transact log summary page with the json files using apache drill.
-
-# Drill is used to provide scability options in the future.
-
-from jinja2 import Template
-
-import datetime
-import sys
-import subprocess
-import argparse
 import pandas
-
 from pydrill.client import PyDrill
 
-OK           = 0
-WARNING      = 1
-CRITICAL     = 2
-UNKNOWN      = 3
-
 #####################################
-# Header 
+# Header - Don't edit. 
 
 cgitb.enable()
 print("Content-Type: text/html;charset=utf-8\r\n\r\n")
 
 #####################################
-# Body 
+# Body - Edit away. 
 
 print("<h3>Transaction Summary</h3>")
 
