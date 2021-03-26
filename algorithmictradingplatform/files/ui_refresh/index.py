1afd5edeb2dccc8f8f938fff9ef3a3ecb3dfed24
Daniel Neil <danielneilhodder@gmail.com>
2021-03-26 20:16:41 +1100

    Update index.py

--- algorithmictradingplatform/files/ui_refresh/index.py
+++ algorithmictradingplatform/files/ui_refresh/index.py
@@ -2,12 +2,14 @@
 
 import cgitb
 
+#####################################
 # Header 
 
 cgitb.enable()
 print("Content-Type: text/html;charset=utf-8\r\n\r\n")
 
+#####################################
 # Body 
 
-print("Transaction Summary")
+print("<h3>Transaction Summary</h3>")
 
