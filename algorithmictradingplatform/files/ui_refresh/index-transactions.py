bcafdfa76b582fa65486261aaa51fcfe07b2ac67
Daniel Neil <danielneilhodder@gmail.com>
2021-03-29 20:40:15 +1100

    Update index-transactions.py

--- algorithmictradingplatform/files/ui_refresh/index-transactions.py
+++ algorithmictradingplatform/files/ui_refresh/index-transactions.py
@@ -62,7 +62,6 @@ totalVolume = sum(dataframe['quantity'])
 # wrong but change a place holder to come back to
 totalValue = sum(dataframe['quantity'])
 
-
 # Send the HTML headers.
 cgitb.enable()
 print("Content-Type: text/html;charset=utf-8\r\n\r\n")
@@ -80,5 +79,6 @@ with open('/shark/bin/index-transaction.html.jinja') as f:
 	        highest_sell = highest_sell,
 	        lowest_sell = lowest_sell,
 	        totalVolume = totalVolume,
-	        totalValue  = totalValue
+	        totalValue  = totalValue,
+		x = dataframe
 	))
