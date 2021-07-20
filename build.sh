3d7640726c727eeb6e34d5f6f3d7a88e8c4e0efd
Daniel Neil <danielneilhodder@gmail.com>
2021-07-20 17:52:32 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -76,7 +76,7 @@ cp -pr config/ticker-data shark/files/
 
 ##################################################
 # Now run the playbook.
-ansible-playbook ./site-dev.yml -i hosts -o ServerAliveInterval=600
+ansible-playbook ./site-dev.yml -i hosts --ssh-extra-args='-o ServerAliveInterval=600'
 
 ##################################################
 # Generate the configuration
