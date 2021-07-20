459798aa6f71ba427793f5dca5f49c333bab9970
danielneil <you@example.com>
2021-07-20 12:42:15 +1000

    split the site files into prod and dev

--- build.sh
+++ build.sh
@@ -76,7 +76,7 @@ cp -pr config/ticker-data shark/files/
 
 ##################################################
 # Now run the playbook.
-ansible-playbook ./site.yml -i hosts
+ansible-playbook ./site-dev.yml -i hosts
 
 ##################################################
 # Generate the configuration
