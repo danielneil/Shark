696b5b75adfa7f25715c92ba1319585dbf0ca719
Daniel Neil <danielneilhodder@gmail.com>
2021-08-03 12:18:08 +1000

    Update side.php

--- shark/templates/ui_patches/side.php
+++ shark/templates/ui_patches/side.php
@@ -109,44 +109,5 @@
 	</div>
 </div>	
 	
-<div class="navsection">
-	<div class="navsectiontitle">Reports</div>
-	<div class="navsectionlinks">
-		<ul class="navsectionlinks">
-			<li><a href="<?php echo $cfg["cgi_base_url"];?>/avail.cgi" target="<?php echo $link_target;?>">Availability</a></li>
-			<li>
-				<a href="trends.html" target="<?php echo $link_target;?>">Trends</a>
-				<a href="<?php echo $cfg["cgi_base_url"];?>/trends.cgi" target="<?php echo $link_target;?>">(Legacy)</a>
-			</li>
-			<li><a href="<?php echo $cfg["cgi_base_url"];?>/history.cgi?host=all" target="<?php echo $link_target;?>">Alerts</a>
-			<ul>
-				<li><a href="<?php echo $cfg["cgi_base_url"];?>/history.cgi?host=all" target="<?php echo $link_target;?>">History</a></li>
-				<li><a href="<?php echo $cfg["cgi_base_url"];?>/summary.cgi" target="<?php echo $link_target;?>">Summary</a></li>
-				<li>
-					<a href="histogram.html" target="<?php echo $link_target;?>">Histogram</a>
-					<a href="<?php echo $cfg["cgi_base_url"];?>/histogram.cgi" target="<?php echo $link_target;?>">(Legacy)</a>
-				</li>
-			</ul>
-			</li>
-			<li><a href="<?php echo $cfg["cgi_base_url"];?>/notifications.cgi?contact=all" target="<?php echo $link_target;?>">Notifications</a></li>
-			<li><a href="<?php echo $cfg["cgi_base_url"];?>/showlog.cgi" target="<?php echo $link_target;?>">Event Log</a></li>
-		</ul>
-	</div>
-</div>
-	
-<div class="navsection">
-	<div class="navsectiontitle" style="background-color: red; color: white; text-align: center">Development</div>
-	<div class="navsectionlinks">
-		<ul class="navsectionlinks">
-			<li><a href="http://<?php echo $_SERVER['SERVER_ADDR'];?>:15672" target="_blank">RabbitMQ WebUI</a></li>
-			<li><a href="http://<?php echo $_SERVER['SERVER_ADDR'];?>:8047" target="_blank">Apache Drill WebUI</a></li>
-			<li><a href="http://<?php echo $_SERVER['SERVER_ADDR'];?>:8080/nifi" target="_blank">Apache NiFi WebUI</a></li>
-			<li><a href="http://<?php echo $_SERVER['SERVER_ADDR'];?>/shark" target="_blank">Shark Web</a></li>
-			<li><a href="http://<?php echo $_SERVER['SERVER_ADDR'];?>:81" target="_blank">Shark Monitoring</a></li>
-			<li><a href="http://<?php echo $_SERVER['SERVER_ADDR'];?>/dokuwiki/doku.php" target="_blank">Dokuwiki</a></li>
-		</ul>
-	</div>
-</div>
-
 </body>
 </html>
