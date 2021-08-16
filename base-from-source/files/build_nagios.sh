6ccd03f6270e91fe7323d92f92143c171048eb66
Daniel Neil <danielneilhodder@gmail.com>
2021-08-16 19:19:14 +1000

    Update build_nagios.sh

--- base-from-source/files/build_nagios.sh
+++ base-from-source/files/build_nagios.sh
@@ -1,12 +1,10 @@
 cd /tmp/nagios-4.4.6 &&                 
-./configure --with-httpd-conf=/etc/apache2/sites-enabled &&                 
+./configure &&                 
 make all &&                 
-make install-groups-users &&                 
-usermod -a -G nagios www-data && 
+make install-groups-users &&
+usermod -a -G nagios apache &&
 make install &&  
 make install-daemoninit &&  
 make install-commandmode &&                 
 make install-config &&  
-make install-webconf &&  
-a2enmod rewrite &&  
-a2enmod cgi
+make install-webconf 
