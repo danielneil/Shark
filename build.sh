6b22da6704fe82a19600186197061a36e6f639a2
Daniel Neil <danielneilhodder@gmail.com>
2022-01-05 16:11:50 +1100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #/usr/bin/bash
 
-##################################################
-# Exit when any command fails
-set -e
-
-##################################################
 # Installing Shark.
 ansible-playbook ./site.yml -i hosts --ssh-extra-args='-o ServerAliveInterval=3600'
 
