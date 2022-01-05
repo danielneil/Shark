65c15f888e99a53580d0befde1764671dd151e82
Daniel Neil <danielneilhodder@gmail.com>
2022-01-05 13:37:47 +1100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,14 @@
 set -e
 
 ##################################################
-# Run the playbook.
+# Installing Shark.
 ansible-playbook ./site.yml -i hosts --ssh-extra-args='-o ServerAliveInterval=3600'
 
-# Installing the plugins.
+# Installing the Shark Plugins.
 ansible-playbook /var/www/shark-git/Shark-Plugins/site.yml -i hosts
+
+# Verifying Nagios config.
+/usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
+
+# Startin httpd and nagios.
+systemctl start httpd && systemctl start nagios
