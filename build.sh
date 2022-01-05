fc58e0e11967c6ef254912fbdbd6a9924ae8bfca
Daniel Neil <danielneilhodder@gmail.com>
2022-01-05 13:32:08 +1100

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ set -e
 ##################################################
 # Run the playbook.
 ansible-playbook ./site.yml -i hosts --ssh-extra-args='-o ServerAliveInterval=3600'
+
+# Installing the plugins.
+ansible-playbook /var/www/shark-git/Shark-Plugins/site.yml -i hosts
