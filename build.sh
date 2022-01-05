19e8d9687aa30836f86b59cf09f16f1a467452ae
Daniel Neil <danielneilhodder@gmail.com>
2022-01-05 19:56:36 +1100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@ if ! `which figlet > /dev/null` ; then
   dnf install figlet -y > /dev/null
 fi
 /usr/bin/figlet Shark
-echo "The Algorithmic Trading Platform..."
+printf "The Algorithmic Trading Platform..."
 
-echo "\nInstalling Shark..."
+printf "\nInstalling Shark..."
 ansible-playbook ./site.yml -i hosts --ssh-extra-args='-o ServerAliveInterval=3600'
 
-echo "\nInstalling the Shark Plugins..."
+printf "\nInstalling the Shark Plugins..."
 ansible-playbook /shark/Shark-Plugins/site.yml -i hosts
 
-echo "\nStarting httpd and nagios..."
+printf "\nStarting httpd and nagios..."
 systemctl restart httpd && systemctl restart nagios
 
-echo "\nVerifying Shark config..."
+printf "\nVerifying Shark config..."
 /usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
