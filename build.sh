cc3fbdeecf280a703f78e742e85982487cd59866
Daniel Neil <danielneilhodder@gmail.com>
2022-01-05 19:57:06 +1100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,14 +6,14 @@ fi
 /usr/bin/figlet Shark
 printf "The Algorithmic Trading Platform..."
 
-printf "\nInstalling Shark..."
+printf "\n\nInstalling Shark..."
 ansible-playbook ./site.yml -i hosts --ssh-extra-args='-o ServerAliveInterval=3600'
 
-printf "\nInstalling the Shark Plugins..."
+printf "\n\nInstalling the Shark Plugins..."
 ansible-playbook /shark/Shark-Plugins/site.yml -i hosts
 
-printf "\nStarting httpd and nagios..."
+printf "\n\nStarting httpd and nagios..."
 systemctl restart httpd && systemctl restart nagios
 
-printf "\nVerifying Shark config..."
+printf "\n\nVerifying Shark config..."
 /usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
