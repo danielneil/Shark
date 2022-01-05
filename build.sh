2fbe9b89ec6617b85c02129f19d0a3ed5456d020
root <root@172-105-176-214.ip.linodeusercontent.com>
2022-01-05 20:45:51 +1100

    prettyd up the build script

--- build.sh
+++ build.sh
@@ -3,8 +3,16 @@
 if ! `which figlet > /dev/null` ; then
   dnf install figlet -y > /dev/null
 fi
+
+if ! `which jp2a > /dev/null` ; then
+ dnf -y install https://tchung.org/jp2a/files/jp2a-1.0.8-1.el8.x86_64.rpm
+fi
+
+
 /usr/bin/figlet Shark
 printf "The Algorithmic Trading Platform..."
+printf "\nAuthor: danielneil"
+jp2a shark/files/screenshots/shark.jpg --background=light
 
 printf "\n\nInstalling Shark..."
 ansible-playbook ./site.yml -i hosts --ssh-extra-args='-o ServerAliveInterval=3600'
@@ -16,4 +24,3 @@ printf "\n\nStarting httpd and nagios..."
 systemctl restart httpd && systemctl restart nagios
 
 printf "\n\nVerifying Shark config..."
-/usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
